bed8a9f995be533c8ac034922b205263d0a85dc2
Grace Webb <nextechcatapult@Joshs-iPhone.localdomain>
2018-06-25 14:13:42 -0400

    life

---
+++ main.js
@@ -0,0 +1,19 @@
+// display the numbers between 1 and 20
+
+// display the even numbers from 1 to 20
+
+//display the odd numbers from 1 to 20
+
+//display the multiples of 5 up to 100
+
+//display the square numbers up to 100
+
+//display the numbers counting backwords from 20 to 1
+
+//display the even numbers counting backwords from 20
+
+//display the odd numbers from 20 to 1, backwords
+
+//display the multiples of 5, counting down from 100
+
+//display the square numbers, counting down from 100
