cc1b954ebe993d0083fc7a0613b2e54f2c7f285b
grace <gracewebb20619@gmail.com>
2018-06-26 11:25:20 -0400

    hey

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 // display the numbers between 1 and 20
 console.log("Display the numbers from 1 to 20");
-for(var i=1; i <=20; i++) {
+for (var i = 1; i <= 20; i++) {
     console.log(i);
 }
 // display the even numbers from 1 to 20
-console.log("Display the even numbers from 1 to 20");
-for(var i=1; i <=20; i/2); {
-console.log(i);
-}
+for(var i=2; i<=20; i=i+2){
+    console.log(i) 
+  }
+
 //display the odd numbers from 1 to 20
-
+for(var i=1; i<=20; i=i+2){
+    console.log(i) 
+  }
 //display the multiples of 5 up to 100
-
+for(var i=5; i<=100; i=i+5){
+    console.log(i) 
+  }
 //display the square numbers up to 100
-
+for(var i=1; i<=100; i=i*2){
+    console.log(i) 
+  }
 //display the numbers counting backwords from 20 to 1
-
+for(var i=20;i>=1;i-=1) {
+    console.log(i);
+  }
 //display the even numbers counting backwords from 20
-
+for(var i=20;i>=1;i-=2) {
+    console.log(i);
+  }
 //display the odd numbers from 20 to 1, backwords
-
+for(var i=19;i>=1;i-=2) {
+    console.log(i);
+  }
 //display the multiples of 5, counting down from 100
-
+for(var i=100;i>=5;i-=5) {
+    console.log(i);
+  }
 //display the square numbers, counting down from 100
+for(var i=100;i>=5;i-=5) {
+    console.log(i);
+  }
